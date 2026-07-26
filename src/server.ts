74cbbce5883c60758ae2f544e0f8ee2984e8455f
Alves <ojvalves@gmail.com>
2026-07-26 19:41:34 -0300

    fix: atualiza mensagem do servidor para teste de PR

--- src/server.ts
+++ src/server.ts
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use(taskRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT} ok`);
 });
